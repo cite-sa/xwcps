c19c1fe65ca2b0611725063c3c2cda7f0b057e56
ikavvour <jhkavv@gmail.com>
2016-04-15 12:03:34 +0300

    #queries

--- xwcps-parser-ui/app/xwcps-parser-app.js
+++ xwcps-parser-ui/app/xwcps-parser-app.js
@@ -23,7 +23,7 @@ parserApp.directive('postRender', ['$timeout', function ($timeout) {
 
 parserApp.directive("selectorClickHandler", function () {
 	return {
-		link: function(scope, element, attrs) {
+		link: function (scope, element, attrs) {
 
 			$(element).click(function () {
 				$(".prettyprinted").empty();
@@ -36,7 +36,7 @@ parserApp.directive("selectorClickHandler", function () {
 
 parserApp.directive("submitClickHandler", function () {
 	return {
-		link: function(scope, element, attrs) {
+		link: function (scope, element, attrs) {
 			$(element).click(function () {
 				scope.result = {};
 				scope.$apply();
@@ -45,15 +45,15 @@ parserApp.directive("submitClickHandler", function () {
 	}
 });
 
-parserApp.directive('resizeQueryForm', function($window) {
-	return function($scope) {
-		$scope.queryDropdownResize = function() {
-			var queryInputWidth =  angular.element(document.querySelector("#queryInput")).width();
+parserApp.directive('resizeQueryForm', function ($window) {
+	return function ($scope) {
+		$scope.queryDropdownResize = function () {
+			var queryInputWidth = angular.element(document.querySelector("#queryInput")).width();
 			var queriesDropdownToggleWidth = angular.element(document.querySelector("#queries-dropdown-toggle")).width();
 			angular.element(document.querySelectorAll('.query-a')).width(queryInputWidth + queriesDropdownToggleWidth + 10);
 		};
 		$scope.queryDropdownResize();
-		return angular.element($window).bind('resize', function() {
+		return angular.element($window).bind('resize', function () {
 			$scope.queryDropdownResize();
 			return $scope.$apply();
 		});
@@ -87,9 +87,9 @@ parserApp.controller("xWCPSExecutorController", function ($scope, $timeout, $htt
 	};
 
 	/*$scope.emptyResult = function() {
-		$scope.result = undefined;
-		scope.$apply();
-	};*/
+	 $scope.result = undefined;
+	 scope.$apply();
+	 };*/
 
 	$scope.prettifyXml = function (value) {
 		if (value == null) {
@@ -97,6 +97,23 @@ parserApp.controller("xWCPSExecutorController", function ($scope, $timeout, $htt
 		}
 		return vkbeautify.xml(value, 4);
 	};
+
+	$scope.getSize = function (result) {
+		var size = 0;
+
+		if (result == undefined) {
+			return 0;
+		}
+
+		if (result.aggregatedValue) {
+			++size;
+		}
+		if (result.mixedValues) {
+			size += result.mixedValues.length;
+		}
+
+		return size;
+	}
 
 	$scope.queries = {};
 	$scope.queries.default =
@@ -111,16 +128,16 @@ parserApp.controller("xWCPSExecutorController", function ($scope, $timeout, $htt
 
 	$scope.queries.all = [
 		"for data in /server[@endpoint='http://access.planetserver.eu:8080/rasdaman/ows']/coverage " +
-			"where describeCoverage(data)//gml:cat_solar_longitude[text()>86.0122] return encode( \n" +
-			"{ red:(int)(255 / (1 - (1.395 / ((1 - ((data.band_61 - data.band_57)/(data.band_72 - data.band_57))) * 1.370 + ((data.band_61 - data.band_57)/(data.band_72 - data.band_57)) * 1.470)))); \n" +
-			"green: (int)(255 / (1 - (1.525 / ((1 - ((data.band_80 - data.band_57)/(data.band_124 - data.band_57))) * 1.367 + ((data.band_80 - data.band_57)/(data.band_124 - data.band_57)) * 1.808)))); \n" +
-			"blue: (int)(255/ (0.5 * (1 - (1.930 / ((1 - ((data.band_142 - data.band_130)/(data.band_163 - data.band_130))) * 1.850 + ((data.band_142 - data.band_130)/(data.band_163 - data.band_130)) * 2.067))) * 0.5 * (1 - (1.985 / ((1 - ((data.band_151 - data.band_130)/(data.band_163 - data.band_130))) * 1.850 + ((data.band_151 - data.band_130)/(data.band_163 - data.band_130)) * 2.067))))); \n" +
-			"alpha: (data.band_100 != 65535) * 255 }, \"png\", \"nodata=null\")",
-			"for data in /server[@endpoint='http://access.planetserver.eu:8080/rasdaman/ows']/coverage return describeCoverage(data)",
+		"where describeCoverage(data)//gml:cat_solar_longitude[text()>86.0122] return encode( \n" +
+		"{ red:(int)(255 / (1 - (1.395 / ((1 - ((data.band_61 - data.band_57)/(data.band_72 - data.band_57))) * 1.370 + ((data.band_61 - data.band_57)/(data.band_72 - data.band_57)) * 1.470)))); \n" +
+		"green: (int)(255 / (1 - (1.525 / ((1 - ((data.band_80 - data.band_57)/(data.band_124 - data.band_57))) * 1.367 + ((data.band_80 - data.band_57)/(data.band_124 - data.band_57)) * 1.808)))); \n" +
+		"blue: (int)(255/ (0.5 * (1 - (1.930 / ((1 - ((data.band_142 - data.band_130)/(data.band_163 - data.band_130))) * 1.850 + ((data.band_142 - data.band_130)/(data.band_163 - data.band_130)) * 2.067))) * 0.5 * (1 - (1.985 / ((1 - ((data.band_151 - data.band_130)/(data.band_163 - data.band_130))) * 1.850 + ((data.band_151 - data.band_130)/(data.band_163 - data.band_130)) * 2.067))))); \n" +
+		"alpha: (data.band_100 != 65535) * 255 }, \"png\", \"nodata=null\")",
+		"for data in /server[@endpoint='http://access.planetserver.eu:8080/rasdaman/ows']/coverage return describeCoverage(data)",
 		"for data in //coverage return describeCoverage(data)//gml:cat_solar_longitude[text()>86.0122]",
 		"for data in //coverage return <p> describeCoverage(data)//gml:cat_solar_longitude[text()>86.0122] </p>",
 		"for data in //coverage return <div><p>describeCoverage(data)//gml:adding_target/text()</p> " +
-			"<p>describeCoverage(data)//gml:cat_solar_longitude/text()</p></div>"];
+		"<p>describeCoverage(data)//gml:cat_solar_longitude/text()</p></div>"];
 });
 
 
